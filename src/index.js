c3d39e741c7dd2f57fd627f3c183ae83e0d41fef
krischanthas <kris.chanthasiriphan@gmail.com>
2019-07-27 12:14:11 -0700

    prototype 1 completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => {
+    return Math.floor(Math.random() * 1001);
+}
+
+const greeting = (userObj) => {
+    return (
+        <div className="container">
+            <h1>Hello {userObj.name}</h1>
+            <h2 className="text-muted">Your lucky number is {userObj.luckyNumber}</h2>
+        </div>
+    )
+}
+
+const userObj = {
+    name: 'Kris',
+    luckyNumber: luckyNumber(),
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
