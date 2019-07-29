5ec72f163f8c41227301ed57682f76ea3e328896
krischanthas <kris.chanthasiriphan@gmail.com>
2019-07-28 18:47:22 -0700

    added stopwatch.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Stopwatch from './components/stopwatch.js';
 
 const App = () => {
   return (
     <div className="container">
       <h1 className="text-center">Stopwatch</h1>
+      <Stopwatch/>
     </div>
   );
 };
